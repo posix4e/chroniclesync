fe296f64aa73be763f64cbb0df4ac64052235c67
Liza Saravia <liza.saraviag@gmail.com>
2025-01-28 13:52:45 +0000

    Homepage integration tests

--- pages/e2e/home.spec.ts
+++ pages/e2e/home.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('homepage should load correctly on staging', async ({ page }) => {
   // Use the staging URL for the test
-  await page.goto('http://localhost:3000/?clientId=test123');
+  await page.goto('https://api-staging.chroniclesync.xyz/?clientId=test123');
 
   await expect(page).toHaveTitle(/ChronicleSync/i);
 

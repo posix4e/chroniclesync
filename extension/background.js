d7008ddb81070dfe4c73e9f51fae82dffb855aed
Alex Newman <posix4e@gmail.com>
2025-02-06 03:15:30 +0000

    switch to debug

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,4 @@
-function logToBackground(message) {
-  chrome.runtime.getBackgroundPage((backgroundPage) => {
-    if (backgroundPage) {
-      backgroundPage.console.debug(message);
-    }
-  });
-}
+function logToBackground(message) { console.debug(message); }
 
 chrome.tabs.onUpdated.addListener((_tabId, changeInfo, _tab) => {
   if (changeInfo.url) {

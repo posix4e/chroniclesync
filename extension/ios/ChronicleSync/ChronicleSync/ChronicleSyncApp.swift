74eb8e5a6df22588ac1aa68a7e6fa10443a0e9ca
Alex Newman <posix4e@gmail.com>
2025-03-15 15:23:18 -0400

    add ios

---
+++ extension/ios/ChronicleSync/ChronicleSync/ChronicleSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicleSyncApp.swift
+//  ChronicleSync
+//
+//  Created by alex newman on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ChronicleSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ extension/ios/ChronicleSync/ChronicleSyncTests/ChronicleSyncTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChronicleSyncTests.swift
+//  ChronicleSyncTests
+//
+//  Created by alex newman on 3/15/25.
+//
+
+import Testing
+
+struct ChronicleSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
